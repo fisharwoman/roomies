905b8a898507235bca5dcb74abb6fcd266b6e627
reggiemcdonald <regmcdonald95@gmail.com>
2019-06-03 11:47:01 -0700

    extended users api to show all households a roommate is in

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,6 +4,9 @@ const db = require('../db');
 
 /* GET users listing. */
 router
+    /**
+     * Get all the user resource URIs
+     */
     .get('/', async function(req, res, next) {
         try {
             const query = "SELECT userID FROM Roommates";
@@ -13,6 +16,9 @@ router
             res.status(400).send("Bad request");
         }
     })
+    /**
+     * add a user to the roommates resource
+     */
     .post('/', async (req,res) => {
         try {
             const query = `INSERT INTO Roommates (name, phoneNo, password, email)` +
@@ -25,6 +31,9 @@ router
             res.status(400).send(e.message);
         }
     })
+    /**
+     * Get the specified user
+     */
     .get('/:userID', async (req,res) => {
         try {
             const query = `SELECT * FROM Roommates WHERE userID = ${req.params.userID}`;
@@ -34,6 +43,9 @@ router
             res.status(400).send(e.message);
         }
     })
+    /**
+     * Remove the specified user
+     */
     .delete('/:userID', async (req,res) => {
         try {
             const query = `DELETE FROM Roommates WHERE userID = '${req.params.userID}'`;
@@ -44,15 +56,28 @@ router
             res.status(400).send(e.message);
         }
     })
+    /**
+     * Update one or more aspects of the specified user
+     */
     .patch('/:userID', async(req,res) => {
         try {
             const query = `UPDATE Roommates SET name = '${req.body.name}', phoneNo = '${req.body.phoneNo}', password = '${req.body.password}',`+
                 ` email = '${req.body.email}' WHERE userID = ${req.params.userID} RETURNING userID, name, phoneNo, email`;
-            console.log(query);
             let result = await db.any(query);
             res.status(200).json(result);
         } catch (e) {
-            console.log(e);
+            res.status(400).send(e.message);
+        }
+    })
+    /**
+     * Get all the households that the specified user is member to
+     */
+    .get('/:userID/households', async (req, res) => {
+        try {
+            const query = `SELECT houseID FROM Household_Roommates WHERE roommateID = ${req.params.userID}`;
+            let result = await db.any(query);
+            res.status(200).json(result);
+        } catch (e) {
             res.status(400).send(e.message);
         }
     });
