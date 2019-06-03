79bb2e024df0d047076596ac97ee7c75f313465f
i5g2b <i5g2b@ugrad.cs.ubc.ca>
2019-06-02 21:09:29 -0700

    more users route

--- server/routes/users.js
+++ server/routes/users.js
@@ -13,12 +13,46 @@ router
             res.status(400).send("Bad request");
         }
     })
+    .post('/', async (req,res) => {
+        try {
+            const query = `INSERT INTO Roommates (name, phoneNo, password, email)` +
+                ` VALUES ('${req.body.name}', '${req.body.phoneNo}', '${req.body.password}', '${req.body.email}')` +
+                `RETURNING userID;`;
+            let result = await db.any(query);
+            res.status(200).send("http://localhost:3000/" + result[0].userid);
+        } catch (e) {
+            console.log(e);
+            res.status(400).send(e.message);
+        }
+    })
     .get('/:userID', async (req,res) => {
         try {
             const query = `SELECT * FROM Roommates WHERE userID = ${req.params.userID}`;
             let result = await db.any(query);
             res.status(200).json(result);
         } catch (e) {
+            res.status(400).send(e.message);
+        }
+    })
+    .delete('/:userID', async (req,res) => {
+        try {
+            const query = `DELETE FROM Roommates WHERE userID = '${req.params.userID}'`;
+            await db.any(query);
+            res.status(200).send();
+        } catch (e) {
+            console.log(e);
+            res.status(400).send(e.message);
+        }
+    })
+    .put('/:userID', async(req,res) => {
+        try {
+            const query = `UPDATE Roommates SET name = '${req.body.name}', phoneNo = '${req.body.phoneNo}', password = '${req.body.password}',`+
+                ` email = '${req.body.email}' WHERE userID = ${req.params.userID} RETURNING userID, name, phoneNo, email`;
+            console.log(query);
+            let result = await db.any(query);
+            res.status(200).json(result);
+        } catch (e) {
+            console.log(e);
             res.status(400).send(e.message);
         }
     });
