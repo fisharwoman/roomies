ff2397597410a17863e72c3cb900501d007b7045
Sara L <saraningliu@gmail.com>
2019-06-06 20:43:04 -0700

    added 3 buttons to management.

--- client/src/components/Management.js
+++ client/src/components/Management.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import ReactDOM from "react-dom";
+import Main from "./LoginComponent";
+
 
 export default class Management extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            addButtonClicked: false
+        }
+    }
+
     render() {
         return (
-            <div>
-                <h2>Household Management</h2>
+            <div className={"Add"}>
+                Households
+                <input type={"button"} name={"AddButton"} onClick={handleAddButton.bind(this)} value={"Add"}/>
+                <input type={"button"} name={"EditButton"} onClick={buttonAction.bind(this)} value={"Edit"}/>
+                <input type={"button"} name={"RemoveButton"} onClick={buttonAction.bind(this)} value={"Remove"}/>
             </div>
-        )
+        );
+
+        function handleAddButton() {
+            this.setState({
+                addButtonClicked: true
+            });
+            alert("add button pressed");
+
+        }
+
+        function buttonAction() {
+            console.log("button pressed console");
+            alert("button pressed alert");
+        }
     }
+
 }
