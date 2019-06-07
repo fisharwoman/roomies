2e9c364395b7a02e1ff2beed4a3117102d116c5b
Sara L <saraningliu@gmail.com>
2019-06-07 00:03:14 -0700

    Added a collapsible + button that alerts user when + form is submitted in households.

--- client/src/components/Collapsible.js
+++ client/src/components/Collapsible.js
@@ -1,25 +1,53 @@
-import * as React from "react";
+import React, {Component} from 'react';
+import {Button, Collapse} from 'react-bootstrap';
+import './Collapsible.css';
+import Form from "react-bootstrap/Form";
 
-class Collapsible extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            open: false
-        };
-        this.togglePanel = this.togglePanel.bind(this);
+class Collapsible extends Component{
+    state={
+        open:false,
+        address: null,
+        name: null,
+        description: null
+    };
+
+    submitAddForm(){
+        alert("form submitted");
     }
-    togglePanel(e){
-        this.setState({open: !this.state.open})
-    }
-    render() {
-        return (<div>
-            <div onClick={(e)=>this.togglePanel(e)} className="header">
-            {this.props.title}</div>
-    {this.state.open ? (
-        <div className="content">
-        {this.props.children}
-        </div>
-        ) : null}
-    </div>);
+
+    render(){
+        return(
+            <div
+
+                className= "container">
+                <Form className="form">
+                    Add a Household <br/>
+
+                    <label htmlFor={"Address"}>Household Address: </label>
+                    <input type={"text"} name={"address"} placeholder={"555 Some Street"}
+                           onChange={event => {this.setState({address: event.target.value})}} /> <br/>
+
+                    <label htmlFor={"Name"}>Household Name: </label>
+                    <input type={"text"} name={"name"} placeholder={"My Household"}
+                           onChange={event => {this.setState({name: event.target.value})}} /> <br/>
+
+                    <label htmlFor={"Description"}>Household Description: </label>
+                    <input type={"text"} name={"description"}
+                           onChange={event => {this.setState({description: event.target.value})}} /> <br/>
+
+                    <input type={"button"} name={"submitAddForm"} value={"+"}
+                           onClick={this.submitAddForm.bind(this)} /> <br/>
+                </Form>
+
+                <Collapse in={this.state.open}>
+                    <div>
+                        <p>Content when the button is clicked</p>
+                    </div>
+                </Collapse>
+
+            </div>
+        );
     }
 }
+
+export default Collapsible
--- client/src/components/Management.js
+++ client/src/components/Management.js
@@ -1,33 +1,38 @@
 import React from 'react';
-import ReactDOM from "react-dom";
-import Main from "./LoginComponent";
+import Collapsible from "./Collapsible";
+import LoginComponent from "./LoginComponent";
+import Button from "react-bootstrap/Button";
 
 
 export default class Management extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            addButtonClicked: false
-        }
+            showAddCollapsible: false,
+        };
+        this.onAddClick = this.onAddClick.bind(this);
+    }
+
+    onAddClick() {
+        this.setState({
+            showAddCollapsible: true,
+        });
     }
 
     render() {
         return (
             <div className={"Add"}>
                 Households
-                <input type={"button"} name={"AddButton"} onClick={handleAddButton.bind(this)} value={"Add"}/>
+                <input type={"button"} name={"AddButton"} onClick={this.onAddClick.bind(this)} value={"Add"}/>
                 <input type={"button"} name={"EditButton"} onClick={buttonAction.bind(this)} value={"Edit"}/>
                 <input type={"button"} name={"RemoveButton"} onClick={buttonAction.bind(this)} value={"Remove"}/>
+                {this.state.showAddCollapsible ?
+                    <Collapsible /> :
+                    null
+                }
             </div>
         );
 
-        function handleAddButton() {
-            this.setState({
-                addButtonClicked: true
-            });
-            alert("add button pressed");
-
-        }
 
         function buttonAction() {
             console.log("button pressed console");
