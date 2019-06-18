5188c0c7a7cf7aed8ffc0c5273a182448c0858fe
reggiemcdonald <regmcdonald95@gmail.com>
2019-06-17 22:46:22 -0700

    implemented a csv writer

--- server/scripts/makeViews.sql
+++ server/scripts/makeViews.sql
@@ -3,3 +3,9 @@ select expensetypeid, description as type_description, category from expensetype
 
 CREATE OR REPLACE VIEW expenses_with_categories AS
 select * FROM expenses left join expense_types_altered on expenses.expensetype = expense_types_altered.expensetypeid;
+
+create or replace view user_expense_report as
+select e.expensedate, e.amount, e.description as expensedescription, e.createdby, t.description as typedescription,
+c.description as categorydescription
+from expenses e, expensetypes t, expensecategories c
+where e.expensetype = t.expensetypeid and t.category = c.categoryid
